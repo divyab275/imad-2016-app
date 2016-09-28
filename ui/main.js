d04d93c1bf78a16486752603317facf7077b027f
divyab275 <divyab275@gmail.com>
2016-09-28 16:17:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,4 @@
-console.log('Loaded!');
 var img= document.getElementById('onam');
-img.onclick=function() {
-    img.style.marginLeft='300px';
+img.onclick= function() {
+    img.style.marginLeft= '100px';
 };
