f24e6d409401d548d8069c236863e4b99d1463ab
divyab275 <divyab275@gmail.com>
2016-10-02 22:40:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
-var button=document.getElementbyId('counter');
+var button=document.getElementById('counter');
 var counter=0;
 button.onclick=function(){
   counter=counter+1;
-  var span=document.getElementbyId('count');
+  var span=document.getElementById('count');
   span.innerHTML=counter.toString();
 };
